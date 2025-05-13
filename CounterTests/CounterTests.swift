778992a46ccfc540f621c566ea8a299834b48ff4
Murray Buchanan <murray.buchanan.2022@uni.strath.ac.uk>
2025-05-13 19:31:52 +0100

    Initial Commit

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,16 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Murray Buchanan on 13/05/2025.
+//
+
+import Testing
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
